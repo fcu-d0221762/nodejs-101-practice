fdea1a5e3197162ef5c2f932b101c3f36c0d3168
jayonv <kiyoto_zy@hotmail.com>
2018-01-02 00:15:35 +0800

    update

--- day2/99.js
+++ day2/99.js
@@ -1,9 +1,6 @@
 for (var i = 1; i <= 9; i++) {
     for (var j = 1; j <= 9; j++) {
-        (function (numi, numj) {
-            setTimeout(function () {
-                console.log(numi + "*" + numj + "=" + numi * numj);
-            }, 1000 * (i + 1));
-        })(i, j);
+        process.stdout.write(i + "*" + j + "=" + i * j);
     }
+    process.stdout.write("\n");
 }
